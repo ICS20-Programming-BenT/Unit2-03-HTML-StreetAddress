c03491e64b6adc8faf2622947b7d27ebff023b99
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-03-10 00:45:38 +0000

    Updated index.html

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Ben Thomson All rights reserved
+//
+// Created by: Ben Thomson
+// Created on: 03/09/2023
+// This file contains the JS functions for index.html
+
+function enterClicked () {
+  // This function gets user's street name and street number and displays it back to user.
+
+  // get street number from text field and cast it to integer
+  let streetNumber = parseInt(document.getElementById("street-number").value)
+  // get street name from text field
+  let streetName = document.getElementById("street-name").value
+
+  // display name and age back to user
+  document.getElementById('user-info').innerHTML = "Your street number is " + streetNumber + " and your street name is " + streetName + "."
+}
